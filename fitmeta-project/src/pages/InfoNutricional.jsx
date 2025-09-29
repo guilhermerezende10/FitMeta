dac40a49c3eb7eae31f6314950c4214fb66550f1
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-09-29 19:52:46 -0300

    Update InfoNutricional.jsx

--- fitmeta-project/src/pages/InfoNutricional.jsx
+++ fitmeta-project/src/pages/InfoNutricional.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import Title from "../ui/Title";
+
 function InfoNutricional() {
-    return (
-        <div>
-            
-        </div>
-    )
+    useEffect(() => {
+       async function fetchData() {
+        const query = ``;
+        const data = await fetch(query)
+        console.log(data)
+
+       }
+       fetchData()
+    }, [])
+  return (
+    <div>
+      <Title>
+        Informações nutricionais de qualquer alimento
+      </Title>
+    </div>
+  );
 }
 
-export default InfoNutricional
+export default InfoNutricional;
