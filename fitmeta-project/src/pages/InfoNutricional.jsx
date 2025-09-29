a7129eb974d724674ef81557f367a13cf0556e29
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-09-29 20:08:46 -0300

    update info nutricional

--- fitmeta-project/src/pages/InfoNutricional.jsx
+++ fitmeta-project/src/pages/InfoNutricional.jsx
@@ -1,10 +1,15 @@
 import { useEffect } from "react";
 import Title from "../ui/Title";
 
+const apiKey = "46c739a6460f4b81b463a41d56d6f06d"; // Client ID
+const apiKey2 = "d756ddc46c874e28a949ae6a71b1359e"; // Client Secret
+
+
+
 function InfoNutricional() {
     useEffect(() => {
        async function fetchData() {
-        const query = ``;
+        const query = `${apiKey}`;
         const data = await fetch(query)
         console.log(data)
 
