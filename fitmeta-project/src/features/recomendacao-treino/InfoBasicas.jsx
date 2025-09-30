d763103ffa7edf6fa356290bc458161e8f9fd8bd
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-09-30 12:19:13 -0300

    Update InfoBasicas.jsx

--- fitmeta-project/src/features/recomendacao-treino/InfoBasicas.jsx
+++ fitmeta-project/src/features/recomendacao-treino/InfoBasicas.jsx
@@ -19,9 +19,7 @@ function InfoBasicas() {
 
   return (
     <Container>
-      <div className="absolute top-5 justify-center left-1/2 transform -translate-x-1/2 z-10">
-        <Logo className="w-20 h-20" src={logoDarkblue} />
-      </div>
+  
       <div className="bg-[#192126] relative bottom-24 py-4 px-14 rounded-full shadow-md w-full max-w-xl">
         <Title className="text-2xl font-bold text-white">
           Informações Básicas
