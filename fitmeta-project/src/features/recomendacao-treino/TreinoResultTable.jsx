278b52a9c13fa58aa6091f237b5ecd6a5fcc5a1a
ArthuramigodoRezende <arthur19122007@gmail.com>
2025-10-01 19:48:25 -0300

    Revert "Merge branch 'main' of https://github.com/guilhermerezende10/FitMeta"
    This reverts commit 2efb1cebd2c67ab1d699260ab93dbde267128c4f, reversing
    changes made to f0b99dd1ed9d8fb2ea7799759ad3a17c875c77fe.

--- fitmeta-project/src/features/recomendacao-treino/TreinoResultTable.jsx
+++ fitmeta-project/src/features/recomendacao-treino/TreinoResultTable.jsx
@@ -6,23 +6,41 @@ import "swiper/css/bundle";
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import {treinos} from "../../data/data-recomendacao-treino"
+const treinos = [
+    {
+        nome: "PPL(3x) / 45min",
+        segunda: [
+            ['Supino Inclinado', '(4x)'],
+            ['Crucifixo', '(4x)'],
+            ['Elevação Lateral na Máquina ou Polia', '(4x)'],
+            ['Tríceps Francês', '(3x)'],
+            ['Tríceps Pulley', '(3x)']
+        ],
+        terca: "Descanso",
+        quarta: [
+            ['Cadeira Flexora', '(4x)'],
+            ['Agachamento Pêndulo', '(4x)'],
+            ['Mesa Flexora', '(3x)'],
+            ['Cadeira Extensora', '(4x)'],
+            ['Panturrilha no Leg', '(4x)']
+        ],
+        quinta: "Descanso",
+        sexta: [
+            ['Puxada Fechada', '(4x)'],
+            ['Remada Aberta', '(3x)'],
+            ['Remada Baixa', '(3x)'],
+            ['Rosca Scott', '(2x)'],
+            ['Rosca no Banco Inclinado', '(2x)']
+        ],
+        sabado: "Descanso",
+        domingo: "Descanso",
+        faixaRep: "5 a 9"
+    }
+];
 
 function TreinoResultTable() {
-  const { state: {infoBasicas, treinoAnswers} } = useForm();
-  console.log(infoBasicas, treinoAnswers)
-  const treinoFinal = treinos[0];
-
-  // Array dos dias para iterar
-  const diasSemana = [
-    "segunda",
-    "terca",
-    "quarta",
-    "quinta",
-    "sexta",
-    "sabado",
-    "domingo",
-  ];
+  const { state, dispatch } = useForm();
+  const treinoFinal = treinos[0]
 
   return (
     <Swiper
@@ -30,27 +48,11 @@ function TreinoResultTable() {
       className="relative h-screen w-screen overflow-hidden"
       navigation
       pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
       autoplay={{ delay: 8000, disableOnInteraction: true }}
     >
-      {diasSemana.map((dia) => (
-        <SwiperSlide key={dia}>
-          <div className="p-6">
-            <h2 className="text-2xl font-bold capitalize">{dia}</h2>
+      {treinoFinal.map((treino) => (
+        <SwiperSlide key={treino.nome}>
 
-            {Array.isArray(treinoFinal[dia]) ? (
-              <ul className="mt-4 space-y-2">
-                {treinoFinal[dia].map(([exercicio, repeticoes], index) => (
-                  <li key={index} className="flex justify-between">
-                    <span>{exercicio}</span>
-                    <span className="font-semibold">{repeticoes}</span>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p className="mt-4 italic">{treinoFinal[dia]}</p>
-            )}
-          </div>
         </SwiperSlide>
       ))}
     </Swiper>
