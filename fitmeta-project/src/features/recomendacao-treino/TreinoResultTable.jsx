3dd8a3b251ede128ca3c309ee13b48d77b6dd452
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-10-01 09:21:13 -0300

    update

--- fitmeta-project/src/features/recomendacao-treino/TreinoResultTable.jsx
+++ fitmeta-project/src/features/recomendacao-treino/TreinoResultTable.jsx
@@ -6,26 +6,55 @@ import "swiper/css/bundle";
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-const todosTreinos = [
+const treinos = [
     {
         nome: "PPL(3x) / 45min",
-        segunda: ['', '',''],
+        segunda: [
+            ['Supino Inclinado', '(4x)'],
+            ['Crucifixo', '(4x)'],
+            ['Elevação Lateral na Máquina ou Polia', '(4x)'],
+            ['Tríceps Francês', '(3x)'],
+            ['Tríceps Pulley', '(3x)']
+        ],
         terca: "Descanso",
-        quarta: [''],
+        quarta: [
+            ['Cadeira Flexora', '(4x)'],
+            ['Agachamento Pêndulo', '(4x)'],
+            ['Mesa Flexora', '(3x)'],
+            ['Cadeira Extensora', '(4x)'],
+            ['Panturrilha no Leg', '(4x)']
+        ],
         quinta: "Descanso",
-        sexta: [''],
+        sexta: [
+            ['Puxada Fechada', '(4x)'],
+            ['Remada Aberta', '(3x)'],
+            ['Remada Baixa', '(3x)'],
+            ['Rosca Scott', '(2x)'],
+            ['Rosca no Banco Inclinado', '(2x)']
+        ],
         sabado: "Descanso",
         domingo: "Descanso",
-        faixaRep: "5 a 9 Repetições (até a falha) em todos exercícios."
+        faixaRep: "5 a 9"
     }
-]
+];
 
 function TreinoResultTable() {
   const { state, dispatch } = useForm();
+  const treinoFinal = treinos[0]
 
   return (
-    <Swiper>
+    <Swiper
+      slidesPerView={1}
+      className="relative h-screen w-screen overflow-hidden"
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 8000, disableOnInteraction: true }}
+    >
+      {treinoFinal.map((treino) => (
+        <SwiperSlide key={treino.nome}>
 
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 }
