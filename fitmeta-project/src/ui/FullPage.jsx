76760d335bdc0e633c9786004a6a4cdc1072b66a
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-10-07 11:44:19 -0300

    protecting routes

---
+++ fitmeta-project/src/ui/FullPage.jsx
@@ -0,0 +1,9 @@
+function FullPage({ children }) {
+  return (
+    <div className="height-screen bg-brand-bgDarkGray flex items-center content-center">
+      {children}
+    </div>
+  );
+}
+
+export default FullPage;
--- fitmeta-project/src/ui/ProtectedRoute.jsx
+++ fitmeta-project/src/ui/ProtectedRoute.jsx
@@ -1,5 +1,33 @@
-function ProtectedRoute({children}) {
-    return children
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import FullPage from "./FullPage";
+import Spinner from "./Spinner";
+import { useUser } from "../features/authentication/useUser";
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  // 1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  // 2. If there is NO authenticated user, redirect to the /login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 3. While loading, show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  // 4. If there IS a user, render the app
+  if (isAuthenticated) return children;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
