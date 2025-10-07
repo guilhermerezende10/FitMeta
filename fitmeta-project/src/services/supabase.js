b2c3685d55655683a4fb8b154ad9ccb639322bb9
Rafa <137740632+The-Killer-Raf@users.noreply.github.com>
2025-10-07 10:36:44 -0300

    Adding Supabase Connection

--- fitmeta-project/src/services/supabase.js
+++ fitmeta-project/src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://wiwloahicgxcwgnwilwj.supabase.co";
+export const supabaseUrl = "https://xwwwlceaiwutwhqpqfar.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpd2xvYWhpY2d4Y3dnbndpbHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0MTcxMDQsImV4cCI6MjA3MTk5MzEwNH0.f_xggM3YIZHjnxHRsUT7TtXnwiZX9KvgttIeMc5C3n0";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3d3dsY2VhaXd1dHdocXBxZmFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NDIyNjIsImV4cCI6MjA3NTQxODI2Mn0.9seOozQRsndjKydmOmzkU4V2QacsI7zBdGG9ijo9Gps";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
