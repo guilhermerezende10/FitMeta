37b0799ccc06e9cdba72b4981624fdf9075a95a9
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-10-07 13:06:40 -0300

    eye off and on

--- fitmeta-project/src/features/authentication/LoginRegisterInput.jsx
+++ fitmeta-project/src/features/authentication/LoginRegisterInput.jsx
@@ -1,4 +1,5 @@
-import { IoMdEye } from "react-icons/io";
+import { useState } from "react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 function LoginRegisterInput({
   type,
@@ -8,13 +9,19 @@ function LoginRegisterInput({
   onChange,
   value,
   autoComplete,
-  disabled
+  disabled,
 }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <div className="relative w-80">
       <input
         className="w-full bg-gray-200 text-[#2d1748] placeholder-gray-500 py-6 my-2 pl-4 pr-10 rounded-xl outline-none"
-        type={type}
+        type={showPassword ? "text" : type}
         placeholder={placeholder}
         name={name}
         onChange={onChange}
@@ -22,8 +29,17 @@ function LoginRegisterInput({
         autoComplete={autoComplete}
         disabled={disabled}
       />
-      <span className="absolute text-2xl right-3 top-1/2 -translate-y-1/2 text-gray-600 cursor-pointer">
-        {type === "password" ? <IoMdEye /> : iconElement}
+      <span
+        className="absolute text-2xl right-3 top-1/2 -translate-y-1/2 text-gray-600 cursor-pointer"
+        onClick={togglePassword}
+      >
+        {type !== "password" ? (
+          iconElement
+        ) : showPassword ? (
+          <IoMdEyeOff />
+        ) : (
+          <IoMdEye />
+        )}
       </span>
     </div>
   );
