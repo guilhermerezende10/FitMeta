aac3751bbfffef9c9f8af370bb399d7c93eceb6e
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-10-08 09:21:55 -0300

    update api security

--- fitmeta-project/src/services/supabase.js
+++ fitmeta-project/src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://xwwwlceaiwutwhqpqfar.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3d3dsY2VhaXd1dHdocXBxZmFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NDIyNjIsImV4cCI6MjA3NTQxODI2Mn0.9seOozQRsndjKydmOmzkU4V2QacsI7zBdGG9ijo9Gps";
+
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
