f5effd6d401876f5a830f4bdb87c17f4658b9b72
Rafa <137740632+The-Killer-Raf@users.noreply.github.com>
2025-10-08 11:52:12 -0300

    Update apiAuth.js

--- fitmeta-project/src/services/apiAuth.js
+++ fitmeta-project/src/services/apiAuth.js
@@ -35,6 +35,6 @@ export async function getCurrentUser() {
 export async function registerGoogle() {
   console.log('entrou')
   await supabase.auth.signInWithOAuth({
-    provider: "google",
+    provider: 'google',
   });
 }
