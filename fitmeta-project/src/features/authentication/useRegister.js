e62dc1d6b14f54d7f039e2d1f52d6e0bb9d7a4e1
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-10-21 11:02:32 -0300

    Update useRegister.js

--- fitmeta-project/src/features/authentication/useRegister.js
+++ fitmeta-project/src/features/authentication/useRegister.js
@@ -4,11 +4,13 @@ import toast from "react-hot-toast";
 import { register } from "../../services/apiAuth";
 
 export function useRegister() {
+  const navigate = useNavigate();
 
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: ({ email, password }) => register({ email, password }),
     onSuccess: () => {
       toast.success("Conta criada com sucesso!");
+      navigate("/login");
     },
     onError: (err) => {
       toast.error(err.message || "Erro ao criar conta.");
