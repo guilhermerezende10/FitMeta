1ba762d0ca268f367c013b8728b4378ba50d7a58
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-11-28 23:27:24 -0300

    logout button

--- src/pages/Recomendado.jsx
+++ src/pages/Recomendado.jsx
@@ -1,11 +1,13 @@
 import RecomendadoList from "../features/recomendado/RecomendadoList";
 import Logo from "../ui/Logo";
 import Title from "../ui/Title";
+import { TbLogout } from "react-icons/tb";
 
 function Recomendado() {
   return (
     <div className="bg-brand-bgDarkGray min-h-real flex flex-col items-center py-5">
       <Logo className="mb-2 w-20 h-20 object-contain" />
+      <TbLogout  className="mb-2 w-8 h-8 absolute right-5 top-11 text-white" />
 
       <Title className="text-2xl text-center font-bold text-white mb-2">
         Recomendado para você
