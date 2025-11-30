945756a57fee9f89eb04ac174b98d5efb5f2d0bb
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-11-30 14:11:40 -0300

    bugs fixed

--- src/pages/RecomendacaoNutricional.jsx
+++ src/pages/RecomendacaoNutricional.jsx
@@ -8,12 +8,16 @@ import imgRecomendacaoNutricionalPc from "../data/images/recomendacao-nutriciona
 
 function RecomendacaoNutricional() {
   return (
-    <Container className="relative h-real w-screen lg:pl-56 overflow-hidden bg-brand-bgDarkGray">
+    <Container className="relative h-real w-screen overflow-hidden bg-brand-bgDarkGray">
       {/* Imagem com fade */}
-      <div className="relative h-[55vh] md:h-[60vh] lg:h-[65vh] w-full overflow-hidden">
+      <div className="relative h-[60vh] md:h-[60vh] lg:h-[65vh] w-full overflow-hidden">
         {/* Mobile */}
         <div className="block lg:hidden">
-          <Img src={imgRecomendacaoNutricional} alt="Imagem de uma refeição" />
+          <Img
+            src={imgRecomendacaoNutricional}
+            alt="Imagem de uma refeição"
+            style={{ objectPosition: "top center" }}
+          />
         </div>
 
         {/* Desktop */}
@@ -32,15 +36,15 @@ function RecomendacaoNutricional() {
           <Logo className="h-20 w-20" />
         </div>
 
-        {/* Mesma posição do outro (lg:top-80) */}
-        <div className="lg:absolute lg:top-56">
-          <Title className="text-4xl sm:text-4xl md:text-5xl lg:mb-12 lg:text-4xl font-extrabold mb-6 text-white">
+        {/* Mesma posição do outro (lg:top-56) */}
+        <div className="lg:absolute lg:top-56 flex flex-col items-center gap-4">
+          <Title className="text-4xl sm:text-4xl md:text-5xl lg:text-4xl font-extrabold mb-6 text-white">
             Recomendação Nutricional
           </Title>
 
           <Button
             page="/recomendacao-nutricional/formulario/iniciar"
-            className="w-full max-w-xs px-14 lg:px-40 py-5 rounded-full font-medium text-white text-lg lg:text-xl lg:font-semibold shadow-lg transition bg-gradient-to-r from-brand-button1Violet to-brand-button2Purple hover:opacity-90 hover:scale-105 transform"
+            className="w-full max-w-[260px] sm:max-w-[300px] px-6 py-4 rounded-full font-medium text-white text-xl shadow-lg transition bg-gradient-to-r from-brand-button1Violet to-brand-button2Purple hover:opacity-90 hover:scale-105 transform"
           >
             Começar
           </Button>
--- src/pages/RecomendacaoTreino.jsx
+++ src/pages/RecomendacaoTreino.jsx
@@ -8,11 +8,9 @@ import davidLaidPc from "../data/images/david-recomendacao-treinopc.jpg";
 
 function RecomendacaoTreino() {
   return (
-    <Container className="relative min-h-real w-screen overflow-hidden bg-brand-bgDarkGray">
+    <Container className="relative h-real w-screen overflow-hidden bg-brand-bgDarkGray">
       {/* Imagem com fade */}
       <div className="relative h-[60vh] md:h-[60vh] lg:h-[65vh] w-full overflow-hidden">
-
-
         {/* Mobile */}
         <div className="block sm:hidden">
           <Img src={davidLaid} alt="David Laid" />
@@ -29,18 +27,22 @@ function RecomendacaoTreino() {
 
       {/* Conteúdo */}
       <Container className="relative -mt-24 sm:-mt-28 md:-mt-32 lg:-mt-36 pb-20 sm:pb-24 md:pb-28 lg:pb-32 px-4 sm:px-6 md:px-8 lg:px-10 text-white text-center flex flex-col items-center w-full max-w-screen-xl mx-auto">
-        <Logo />
+        <div className="lg:hidden mb-5">
+          <Logo className="h-20 w-20" />
+        </div>
 
-        <Title className="text-3xl sm:text-3xl md:text-4xl font-extrabold mb-6 text-white">
-          Monte seu treino
-        </Title>
+        <div className="lg:absolute lg:top-56 flex flex-col items-center gap-4">
+          <Title className="text-3xl sm:text-3xl md:text-4xl font-extrabold mb-6 text-white">
+            Monte seu treino
+          </Title>
 
-        <Button
-          page="/recomendacao-treino/formulario/iniciar"
-          className="w-full max-w-xs px-12 py-4 rounded-full text-white text-base font-medium shadow-lg transition bg-gradient-to-r from-brand-button1Violet to-brand-button2Purple hover:opacity-90 hover:scale-105 transform"
-        >
-          Começar
-        </Button>
+          <Button
+            page="/recomendacao-treino/formulario/iniciar"
+            className="w-full max-w-xs px-12 py-4 rounded-full text-white text-base font-medium shadow-lg transition bg-gradient-to-r from-brand-button1Violet to-brand-button2Purple hover:opacity-90 hover:scale-105 transform"
+          >
+            Começar
+          </Button>
+        </div>
       </Container>
     </Container>
   );
